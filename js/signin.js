41e7ea12a8fcf4cd082239856abf458f7d990b15
basmalaragib69-cloud <basmalaragib69@gmail.com>
2026-06-29 14:05:20 +0300

    fix signin error

--- js/signin.js
+++ js/signin.js
@@ -1,7 +1,7 @@
 var signInEmail = document.querySelector('#email');
 var signInPassword = document.querySelector('#password');
 var signInButton = document.querySelector('#Button'); 
-var signInAlertMessage = document.querySelector('#signInAlertMessage');
+var signInAlertMessage = document.querySelector('#alertMessage');
 var usersList = [];
 
 if(localStorage.getItem('usersList') != null){
@@ -13,53 +13,71 @@ function signIn(){
     var userEmail = '';
 
     if(usersList.length != 0){
-        for(var i=0; i<usersList.length; i++){
-        if(usersList[i].email.toLowerCase() === signInEmail.value.toLowerCase() &&
-           usersList[i].password === signInPassword.value){
 
-            userEmail = signInEmail.value;
-            getUserNameByEmail(userEmail);
-            window.location.href = 'pages/home.html';
-            signInAlertMessage.classList.remove('d-none');
-            if(signInAlertMessage.classList.contains('text-danger')){
-                signInAlertMessage.classList.remove('text-danger');
+        for(var i = 0; i < usersList.length; i++){
+
+            if(usersList[i].email.toLowerCase() === signInEmail.value.toLowerCase() &&
+               usersList[i].password === signInPassword.value){
+
+                userEmail = signInEmail.value;
+                getUserNameByEmail(userEmail);
+
+                signInAlertMessage.classList.remove('d-none');
+
+                if(signInAlertMessage.classList.contains('text-danger')){
+                    signInAlertMessage.classList.remove('text-danger');
+                }
+
+                signInAlertMessage.classList.add('text-success');
+                signInAlertMessage.innerText = 'Login successful. Redirecting...';
+
+                setTimeout(function(){
+                    window.location.href = 'pages/home.html';
+                },1000);
+
+                return;
             }
-            signInAlertMessage.classList.add('text-success');
-            signInAlertMessage.innerText = 'login successful. Redirecting...';
-            return;
+            else{
+
+                signInAlertMessage.classList.remove('d-none');
+                signInAlertMessage.classList.add('text-danger');
+                signInAlertMessage.innerText = 'Invalid email or password.';
+            }
         }
-        else{
-            signInAlertMessage.classList.remove('d-none');
-            signInAlertMessage.classList.add('text-danger');
-            signInAlertMessage.innerText = 'Invalid email or password.';
-        }
-    }
-    }
-    else{
-            signInAlertMessage.classList.remove('d-none');
-            signInAlertMessage.classList.add('text-danger');
-            signInAlertMessage.innerText = 'Invalid email or password.';
     }
 
+    else{
+
+        signInAlertMessage.classList.remove('d-none');
+        signInAlertMessage.classList.add('text-danger');
+        signInAlertMessage.innerText = 'Invalid email or password.';
+    }
 
 }
+
 
 if(signInButton != null){
+
     signInButton.addEventListener('click', function(e){
-    e.preventDefault();
-    signIn();
-})
+
+        e.preventDefault();
+        signIn();
+
+    })
+
 }
+
+
 
 function getUserNameByEmail(email){
 
-    for(var i=0; i<usersList.length; i++){
-        if(usersList[i].email.toLowerCase() === email.toLowerCase()){ 
+    for(var i = 0; i < usersList.length; i++){
+
+        if(usersList[i].email.toLowerCase() === email.toLowerCase()){
 
             localStorage.setItem('loggedInUserName', usersList[i].name);
-   
+
             return;
-          
         }
-    }   
+    }
 }
